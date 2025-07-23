13e4e5fc1a3454944a1eccdb886d8cd061c997f5
Jessica Hdz <jes_22kap@hotmail.com>
2025-07-23 13:19:03 -0600

    Práctica 2.3: fondo negro, texto blanco y cambios en App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ backgroundColor: 'black', color: 'white', height: '100vh', padding: '2rem' }}>
+      <h1>Esta es mi práctica 2.3 integración de GitHub</h1>
+      <p>Y también hice cambios en App.js</p>
+    </div>
+  );
+}
+
+export default App;
